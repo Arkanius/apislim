fc0ef5b892b45156552239db858e698c7f850cd1
Gazotti <vtr.gomes@hotmail.com>
2015-07-09 20:05:58 -0300

    commit final

--- index.php
+++ index.php
@@ -16,6 +16,10 @@ $app->get('/', function () {
 
 $app->get('/categorias','getCategorias');
 $app->post('/produtos','addProduto');
+$app->get('/produtos/:id','getProduto');
+$app->post('/produtos/:id','saveProduto');
+$app->delete('/produtos/:id','deleteProduto');
+$app->get('/produtos','getProdutos');
 
 $app->run();
 
@@ -50,4 +54,59 @@ function addProduto(){
     $stmt->execute();
     $produto->id = $conn->lastInsertId();
     echo json_encode($produto);
+}
+
+function getProduto($id)
+
+{
+    $conn = getConn();
+    $sql = "SELECT * FROM produtos WHERE id=:id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam("id",$id);
+    $stmt->execute();
+    $produto = $stmt->fetchObject();
+
+//categoria
+    $sql = "SELECT * FROM categorias WHERE id=:id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam("id",$produto->idCategoria);
+    $stmt->execute();
+    $produto->categoria = $stmt->fetchObject();
+
+    echo json_encode($produto);
+}
+
+function saveProduto($id)
+{
+    $request = \Slim\Slim::getInstance()->request();
+    $produto = json_decode($request->getBody());
+    $sql = "UPDATE produtos SET nome=:nome,preco=:preco,dataInclusao=:dataInclusao,idCategoria=:idCategoria WHERE   id=:id";
+    $conn = getConn();
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam("nome",$produto->nome);
+    $stmt->bindParam("preco",$produto->preco);
+    $stmt->bindParam("dataInclusao",$produto->dataInclusao);
+    $stmt->bindParam("idCategoria",$produto->idCategoria);
+    $stmt->bindParam("id",$id);
+    $stmt->execute();
+
+    echo json_encode($produto);
+
+}
+
+function deleteProduto($id)
+{
+    $sql = "DELETE FROM produtos WHERE id=:id";
+    $conn = getConn();
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam("id",$id);
+    $stmt->execute();
+    echo "{'message':'Produto apagado'}";
+}
+
+function getProdutos(){
+    $sql = "SELECT *,Categorias.nome as nomeCategoria FROM Produtos,Categorias WHERE Categorias.id=Produtos.idCategoria";
+        $stmt = getConn()->query($sql);
+        $produtos = $stmt->fetchAll(PDO::FETCH_OBJ);
+        echo '{\'produtos\':'.json_encode($produtos).'}';
 }
